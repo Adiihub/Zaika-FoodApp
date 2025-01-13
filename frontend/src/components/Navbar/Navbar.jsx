b54feca5e297d1593a1dcddf4cf09d0002d2e66f
Aditi Kumari <aditi22202@gmail.com>
2025-01-13 19:31:29 +0530

    Frontend Complete

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,19 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { assets } from '../../assets/assets'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
+import { StoreContext } from '../../context/StoreContext'
 
 const Navbar = ({setShowLogin}) => {
 
     const [menu, setMenu] = useState("home");
+    const {getTotalCartAmount} = useContext(StoreContext);
     
   return (
     <div className='navbar'>
-        <img src={assets.logo} alt="" />
+        <Link to='/'><img src={assets.logo} alt="" /></Link>
         <ul className='navbar-menu'>
-            <Link to='/' onClick={() => setMenu("home")} className={menu==="home"?"active":""}>home</Link>
+            <Link to='/' onClick={() => setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
             <a href='#explore-menu' onClick={() => setMenu("menu")} className={menu==="menu"?"active":""}>menu</a>
             <a href='#app-download' onClick={() => setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>mobile-app</a>
             <a href='#footer' onClick={() => setMenu("contact-us")} className={menu==="contact-us"?"active":""}>contact us</a>
@@ -19,8 +21,8 @@ const Navbar = ({setShowLogin}) => {
         <div className='navbar-right'>
             <img src={assets.search_icon} alt="" />
             <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
-                <div className='dot'></div>
+                <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+                <div className={getTotalCartAmount() === 0 ? "" :  "dot"}></div>
             </div>
             <button onClick={() => setShowLogin(true)}>Sign in</button>
         </div>
